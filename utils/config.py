2c30451bc4f28aca94938e5812abdb410217b7d0
Aleksandr Sokolov <bigpewm@gmail.com>
2022-10-30 20:01:28 +0300

    fix: for VB

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,6 @@
 import os
 
 VM_ADDRESS = os.popen("prlctl list -f | grep 'Override' | awk '{print $3}'").readline().replace('\n', '')
+if not VM_ADDRESS:
+    VM_ADDRESS = '0.0.0.0'
 VM_PORT = 4242
